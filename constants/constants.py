ab9e5262eb999b12432e76fa8928c6d10dac1bb5
Bhuvaneshwaran I <bhpo00001@stud.uni-saarland.de>
2023-01-14 03:23:17 +0100

    Added Unity Service constants

--- constants/constants.py
+++ constants/constants.py
@@ -5,3 +5,8 @@ FLASK_PORT = 5002
 
 ## Airsim
 CAPTURE_FOLDER = "."
+
+## Unity Service
+NAVMESH_PATH_URL = "http://localhost:8099/navmeshpath/"
+REQUEST_TEMPLATE = "{{'start_position': {{'x':{start_x},'y':{start_y},'z':{start_z}}},'end_position': {{'x':{end_x},'y':{end_y},'z':{end_z}}}}}"
+headers = {'Content-Type': 'application/json'}
