b0d62c224d6ecdcf8a470003271e50ca6519ce78
Bhuvaneshwaran I <bhpo00001@stud.uni-saarland.de>
2023-01-20 12:31:51 +0100

    Added functions to fetch position of symbolic location by using object_mapping

--- apis/service/UnityService.py
+++ apis/service/UnityService.py
@@ -1,17 +1,53 @@
+import json
+
 import requests
-from constants.constants import NAVMESH_PATH_URL, REQUEST_TEMPLATE, headers
+from constants.constants import NAVMESH_PATH_URL, NAVMESH_REQUEST_TEMPLATE, headers, GET_OBJECTS_REQUEST_TEMPLATE, \
+    OBJECTS_URL
+from constants.object_mapping import object_name_for_symbolic_position
+
+cached_positions = {}
 
 
 def get_navmesh_path(start_x: float, start_y: float, start_z: float, end_x: float, end_y: float, end_z: float):
-    message = REQUEST_TEMPLATE.format(start_x=str(start_x), start_y=str(start_y),
-                                                             start_z=str(start_z),
-                                                             end_x=str(end_x), end_y=str(end_y), end_z=str(end_z))
+    message = NAVMESH_REQUEST_TEMPLATE.format(start_x=str(start_x), start_y=str(start_y),
+                                              start_z=str(start_z),
+                                              end_x=str(end_x), end_y=str(end_y), end_z=str(end_z))
     print(message)
     response = requests.request("POST", NAVMESH_PATH_URL, headers=headers,
                                 data=message)
     print("Response:", response.text)
     return response.text
 
+
+def get_objects(name: str):
+    message = GET_OBJECTS_REQUEST_TEMPLATE.format(name=name)
+    print(message)
+    response = requests.request("POST", OBJECTS_URL, headers=headers, data=message)
+    print("Response:", response.text)
+    return response.text
+
+
+def get_positions_for_symbolic_location(symbolic_location: str):
+    object_name, id = symbolic_location.split("_")
+    if object_name in cached_positions:
+        if id in cached_positions[object_name]:
+            return cached_positions
+    name = get_object_name_from_symbolic_name(object_name)
+    objects = json.loads(get_objects(name))
+    object_position = {}
+    for object in objects:
+        _, id = object["name"].split("_")
+        position = object["position"]
+        object_position[id] = position
+    cached_positions[object_name] = object_position
+    return cached_positions
+
+def get_position_for_symbolic_location(symbolic_location: str):
+    object_name, id = symbolic_location.split("_")
+    positions = get_positions_for_symbolic_location(symbolic_location)
+    return positions[object_name][id]['x'], positions[object_name][id]['y'], positions[object_name][id]['z']
+def get_object_name_from_symbolic_name(object_name):
+    return object_name_for_symbolic_position[object_name]
 
 # def get_navmesh_path(data):
 #     response = requests.request("POST", NAVMESH_PATH_URL, headers=headers,
---
+++ constants/object_mapping.py
@@ -0,0 +1,6 @@
+## Object Name for Symbolic Position
+object_name_for_symbolic_position = {
+    "shelf": "ShelvingRackRandom(Clone)",
+    "box": "box001(Clone)",
+    "launchpad": "launchpad",
+}
