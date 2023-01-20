760ecaebbe0b6662c92ad5ca9627e403847a430b
Bhuvaneshwaran I <bhpo00001@stud.uni-saarland.de>
2023-01-20 12:30:37 +0100

    Receiving data from AJAN and processing it

--- apis/service/AJANPlugin.py
+++ apis/service/AJANPlugin.py
@@ -1,1 +1,22 @@
-# import UnityEngine
+import rdflib
+from rdflib.namespace import FOAF
+
+
+def parse_and_get_actions(data_string, fmt):
+    g = rdflib.Graph()
+    g.parse(data=data_string, format=fmt)
+    actions = []
+    for s, p, o in g:
+        action_URI = rdflib.term.URIRef('http://www.ajan.de/behavior/mdp-ns#hasAction')
+        if p == action_URI:
+            action = str(o)
+            action = action[1:action.__len__()-1]
+            action = action.split(";, ")
+            if action.__len__() == 1:
+                action = action[0].replace(";", "")
+                actions.append(action)
+            else:
+                for a in action:
+                    a = a.replace(";", "")
+                    actions.append(a)
+    return actions
