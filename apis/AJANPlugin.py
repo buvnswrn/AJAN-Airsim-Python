f6357734dad25f9edcce40b4f8cfc3e9c7ca6429
Bhuvaneshwaran I <bhpo00001@stud.uni-saarland.de>
2023-01-20 12:37:24 +0100

    Added endpoint to execute actions given by AJAN

--- apis/AJANPlugin.py
+++ apis/AJANPlugin.py
@@ -1,9 +1,13 @@
 from flask_restx import Namespace, Resource
-from flask import Response
-
+from flask import Response, request
+from .service import AJANPlugin, airsim_controller, UnityService
+from constants import constants
 
 ajan_plugin_ns = Namespace('ajan_plugin', description="AJAN Plugin for processing RDF Input from AJAN Service "
                                                       "and control Drone using Airsim")
+
+
+
 
 
 @ajan_plugin_ns.route('/execute_actions')
@@ -12,4 +16,33 @@ class ExecuteActions(Resource):
     @ajan_plugin_ns.doc(description="Execute actions from RDF Input")
     def post(self):
         # TODO: receive RDF input from AJAN service
+        data = str(request.data.decode('utf-8'))
+        actions_array = AJANPlugin.parse_and_get_actions(data, "turtle")
+        executeActions(actions_array)
+        print(actions_array)
         return Response(status=200)
+
+
+def executeActions(actions_array):
+    for actions in actions_array:
+        if actions.__contains__("do"):
+            action = actions[3:actions.__len__()-1]
+            robot, action = action.split(",")
+            action = action.lstrip()
+            if action == "take_off":
+                airsim_controller.takeoff()
+            elif action == "land":
+                airsim_controller.land()
+            elif action == "capture_image":
+                airsim_controller.captureImage(constants.CAPTURE_FOLDER)
+        elif actions.__contains__("moveto"):
+            robot, action = actions[7:actions.__len__()-1].split(",")
+            action = action.lstrip()
+            x, y, z = UnityService.get_position_for_symbolic_location(action)
+            airsim_controller.move(x, y, z, 1)
+        else:
+            print("Action not supported: " + actions)
+
+
+
+
