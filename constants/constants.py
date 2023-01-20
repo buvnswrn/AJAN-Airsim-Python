e94bd296636cc521a81970f93499475d2ede859e
Bhuvaneshwaran I <bhpo00001@stud.uni-saarland.de>
2023-01-20 12:36:57 +0100

    Added Main URL and Objects URL, Template

--- constants/constants.py
+++ constants/constants.py
@@ -4,9 +4,12 @@ FLASK_HOST = "0.0.0.0"
 FLASK_PORT = 5002
 
 ## Airsim
-CAPTURE_FOLDER = "."
+CAPTURE_FOLDER = "./imgs/"
 
 ## Unity Service
-NAVMESH_PATH_URL = "http://localhost:8099/navmeshpath/"
-REQUEST_TEMPLATE = "{{'start_position': {{'x':{start_x},'y':{start_y},'z':{start_z}}},'end_position': {{'x':{end_x},'y':{end_y},'z':{end_z}}}}}"
+URL = "http://localhost:8099/"
+NAVMESH_PATH_URL = URL + "navmeshpath/"
+OBJECTS_URL = URL + "getObjects/"
+NAVMESH_REQUEST_TEMPLATE = "{{'start_position': {{'x':{start_x},'y':{start_y},'z':{start_z}}},'end_position': {{'x':{end_x},'y':{end_y},'z':{end_z}}}}}"
+GET_OBJECTS_REQUEST_TEMPLATE = "{{'objectOfInterest':'{name}'}}"
 headers = {'Content-Type': 'application/json'}
